26c931147be086ae4bcf84f5978b60dbb7bb7fc5
Elizabeth <leezeelola@gmail.com>
2020-08-05 11:31:38 +0100

    simple animation

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-var imModal = document.getElementById("img-modal");
+var modal = document.getElementById('myModal')
+var imModal = document.getElementById('img-modal')
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var img = document.getElementById("myImg");
+var btn = document.getElementById('myBtn')
+var img = document.getElementById('myImg')
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0]
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = 'block'
+  modal.style.animationPlayState = 'running'
 }
-img.onclick = function() {
-    imModal.style.display = "block";
+img.onclick = function () {
+  imModal.style.display = 'block'
+  imModal.style.animationPlayState = 'running'
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+  modal.style.display = 'none'
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target === modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none'
   }
   if (event.target === imModal) {
-    imModal.style.display = "none";
+    imModal.style.display = 'none'
     // document.getElementById("img-cover").style.display = 'none';
   }
 }
